2819ac5f26866f6285b72a36b203a618de8a93fd
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-01-12 10:32:17 -0500

    fixing hound errors

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -8,73 +8,38 @@ SimpleForm.setup do |config|
   config.wrappers :default,
                   class: :input,
                   hint_class: :field_with_hint,
-                  error_class: :field_with_errors do |b|
-    ## Extensions enabled by default
-    # Any of these extensions can be disabled for a
-    # given input by passing: `f.input EXTENSION_NAME => false`.
-    # You can make any of these extensions optional by
-    # renaming `b.use` to `b.optional`.
+                  error_class: :field_with_errors
+                  do |b|
 
-    # Determines whether to use HTML5 (:email, :url, ...)
-    # and required attributes
     b.use :html5
 
-    # Calculates placeholders automatically from I18n
-    # You can also pass a string as f.input placeholder: "Placeholder"
     b.use :placeholder
 
-    ## Optional extensions
-    # They are disabled unless you pass `f.input EXTENSION_NAME => true`
-    # to the input. If so, they will retrieve the values from the model
-    # if any exists. If you want to enable any of those
-    # extensions by default, you can change `b.optional` to `b.use`.
-
-    # Calculates maxlength from length validations for string inputs
     b.optional :maxlength
 
-    # Calculates pattern from format validations for string inputs
     b.optional :pattern
 
-    # Calculates min and max from length validations for numeric inputs
     b.optional :min_max
 
-    # Calculates readonly automatically from readonly attributes
     b.optional :readonly
 
-    ## Inputs
     b.use :label_input
     b.use :hint,  wrap_with: { tag: :span, class: :hint }
     b.use :error, wrap_with: { tag: :span, class: :error }
 
-    ## full_messages_for
-    # If you want to display the full error message for the attribute, you can
-    # use the component :full_error, like:
-    #
-    # b.use :full_error, wrap_with: { tag: :span, class: :error }
   end
 
-  # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :default
 
-  # Define the way to render check boxes / radio buttons with labels.
-  # Defaults to :nested for bootstrap config.
-  #   inline: input + label
-  #   nested: label > input
   config.boolean_style = :nested
 
-  # Default class for buttons
   config.button_class = 'btn'
 
-
-  # Default tag used for error notification helper.
   config.error_notification_tag = :div
 
-  # CSS class to add for error notification helper.
   config.error_notification_class = 'error_notification'
 
-
   config.browser_validations = false
-
 
   config.boolean_label_class = 'checkbox'
 
