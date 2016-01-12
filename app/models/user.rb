5a323698aa98ad0e249e9350fdc7bcd46ce7baaf
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-01-12 13:41:41 -0500

    fixing hound errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :trackable,
-         :validatable
+                    :registerable,
+                    :recoverable,
+                    :rememberable,
+                    :trackable,
+                    :validatable
 end
