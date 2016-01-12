b399a9ecf6d616b2f1ba4777aefc21b5fce32de8
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-01-12 10:12:53 -0500

    added badges

---
+++ spec/spec_helper.rb
@@ -0,0 +1,35 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+require 'capybara/rspec'
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:suite) do
+  end
+end
