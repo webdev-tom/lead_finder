f2fdf47f133d6724780f1b8bceb9867b6c2dabb7
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-01-25 17:41:41 -0500

    fixing hound violations

--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe Lead, type: :model do
   end
 
   it { should have_valid(:company_name).when('Rand Whitney') }
-  it { should_not have_valid(:company_name).when(nil,'') }
+  it { should_not have_valid(:company_name).when(nil, '') }
 
   it { should have_valid(:phys_address).when('116 Birch Dr.') }
-  it { should_not have_valid(:phys_address).when(nil,'') }
+  it { should_not have_valid(:phys_address).when(nil, '') }
 
   it { should have_valid(:phys_city).when('Boston') }
-  it { should_not have_valid(:phys_city).when(nil,'','45') }
+  it { should_not have_valid(:phys_city).when(nil, '', '45') }
 
   it { should have_valid(:phys_state).when('MA') }
-  it { should_not have_valid(:phys_state).when(nil,'') }
+  it { should_not have_valid(:phys_state).when(nil, '') }
 
   it { should have_valid(:phys_zip).when('01520') }
-  it { should_not have_valid(:phys_zip).when(nil,'','0123','000','abcd') }
+  it { should_not have_valid(:phys_zip).when(nil, '', '0123', '000', 'abcd') }
 end
